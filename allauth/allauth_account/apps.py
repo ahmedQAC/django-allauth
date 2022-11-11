25d75f830050d0a4a7cdb2f7902be8bc259abc01
Ahmed Abid Ali <ahmed.abidali@hotmail.com>
2022-11-11 19:19:03 +0000

    changed name and label in apps.py

--- allauth/allauth_account/apps.py
+++ allauth/allauth_account/apps.py
@@ -3,6 +3,7 @@ from django.utils.translation import gettext_lazy as _
 
 
 class AccountConfig(AppConfig):
-    name = "allauth.account"
+    name = "allauth.allauth_account"
+    label = 'allauth_account'  # Change this
     verbose_name = _("Accounts")
     default_auto_field = "django.db.models.AutoField"
